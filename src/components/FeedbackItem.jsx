73c32addc782407218ecd31e1cb7fbc401287b4e
Jingmei Yang <jingmeiyang@JingmeideMacBook-Air.local>
2022-05-28 23:48:49 -0400

    feedback item and feedback stats, feedback list

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Card from './shared/Card'
 import PropTypes from 'prop-types'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 
 function FeedbackItem({ item }) {
 
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
+      <button onClick={() => {}} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <button onClick={() => {}} className='edit'>
+        <FaEdit color='purple' />
+      </button>
       <div className='text-display'>{item.text}</div>
     </Card>
     
