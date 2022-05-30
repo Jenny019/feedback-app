a13492ab55e385396fc593d18d9aa462e869c6b2
Jingmei Yang <jingmeiyang@JingmeideMacBook-Air.local>
2022-05-29 22:56:20 -0400

    pre deploy

---
+++ build/precache-manifest.d0ce1c17a65d9f35c130602019ab56bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6113a87f4627c3b77d6",
+    "url": "/static/css/main.d5711f0d.chunk.css"
+  },
+  {
+    "revision": "c6113a87f4627c3b77d6",
+    "url": "/static/js/main.c6113a87.chunk.js"
+  },
+  {
+    "revision": "0b5adef5a8e99730695f",
+    "url": "/static/js/1.0b5adef5.chunk.js"
+  },
+  {
+    "revision": "0d5b211b2c3f91e45d8f",
+    "url": "/static/js/2.0d5b211b.chunk.js"
+  },
+  {
+    "revision": "a95ebad08de026ef45af",
+    "url": "/static/js/runtime~main.a95ebad0.js"
+  },
+  {
+    "revision": "3f2590067056b4f0630d5b360e694fce",
+    "url": "/static/media/spinner.3f259006.gif"
+  },
+  {
+    "revision": "442d0cf52833378eb5f6ea07dc6ad79c",
+    "url": "/index.html"
+  }
+];
